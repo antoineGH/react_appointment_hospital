2cac6ac5862c4646f9ce2cf75f8a9fb1dbefadc9
Templars <antoine.ratat@gmail.com>
2021-09-04 00:54:13 +0800

    component toast

--- src/components/alertToasts/AlertToasts.js
+++ src/components/alertToasts/AlertToasts.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import { Toast } from 'react-bootstrap'
+import Toast from 'react-bootstrap/Toast'
+import './AlertToast.css'
 
-export default function AlertToasts() {
+export default function AlertToasts(props) {
+	const { title, time, message, show, toggleShowAlert } = props
 	return (
-		<Toast>
+		<Toast show={show} onClose={toggleShowAlert}>
 			<Toast.Header>
-				<img src='holder.js/20x20?text=%20' className='rounded me-2' alt='' />
-				<strong className='me-auto'>Bootstrap</strong>
-				<small>11 mins ago</small>
+				<div className={title === 'Error' ? 'alert_custom alert_danger' : 'alert_custom alert_warning'}></div>
+				<strong className='me-auto'>{title}</strong>
+				<small>{time}</small>
 			</Toast.Header>
-			<Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+			<Toast.Body>{message}</Toast.Body>
 		</Toast>
 	)
 }
--- src/containers/contactPage/ContactPage.js
+++ src/containers/contactPage/ContactPage.js
@@ -13,6 +13,10 @@ export default function ContactPage(props) {
 	const [email, setEmail] = useState('')
 	const [duplicate, setDuplicate] = useState(false)
 	const [countDoctors, setCountDoctors] = useState(contacts.length)
+	const [showAlert, setShowAlert] = useState(false)
+	const [alertTitle, setAlertTitle] = useState('')
+	const [alertMessage, setAlertMessage] = useState('')
+	const [alertTime, setAlertTime] = useState('')
 
 	useEffect(() => {
 		setCountDoctors(contacts.length)
@@ -29,12 +33,25 @@ export default function ContactPage(props) {
 	const handleSubmit = (e) => {
 		e.preventDefault()
 		if (duplicate) {
-			alert('Already exists')
+			setAlertTitle('Error')
+			setAlertMessage('Doctor already exists')
+			setAlertTime('just now')
+			setShowAlert(true)
+			setTimeout(() => {
+				setShowAlert(false)
+			}, 5000)
 			setDuplicate(false)
 			return
 		}
 		if (!firstName || !lastName || !phone || !email) {
-			alert('Missing fields')
+			setAlertTitle('Warning')
+			setAlertMessage('Missing fields')
+			setAlertTime('just now')
+			setShowAlert(true)
+			setTimeout(() => {
+				setShowAlert(false)
+			}, 5000)
+			setDuplicate(false)
 			return
 		}
 		addContact(firstName, lastName, phone, email)
@@ -44,11 +61,16 @@ export default function ContactPage(props) {
 		setEmail('')
 	}
 
+	const toggleShowAlert = () => {
+		setShowAlert(!showAlert)
+	}
+
 	return (
 		<Container className='container_contact'>
 			<CountElement title='Doctor' count={countDoctors} />
 			<TileList items={contacts} removeItems={removeContact} />
-			<AlertToasts />
+			<AlertToasts show={showAlert} toggleShowAlert={toggleShowAlert} title={alertTitle} time={alertTime} message={alertMessage} />
+			<button onClick={handleSubmit}>SUMBIT</button>
 		</Container>
 	)
 }
