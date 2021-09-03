c5ef335d9a3fbdfe16d91a799e86fc8c40c9b3a8
Templars <antoine.ratat@gmail.com>
2021-09-04 01:22:01 +0800

    update toast component contact & appointment page

--- src/components/alertToasts/AlertToasts.js
+++ src/components/alertToasts/AlertToasts.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import Toast from 'react-bootstrap/Toast'
+import ToastContainer from 'react-bootstrap/ToastContainer'
 import './AlertToast.css'
+import { TransitionComponent } from 'react-bootstrap'
+import { Collapse } from 'react-bootstrap'
 
 export default function AlertToasts(props) {
 	const { title, time, message, show, toggleShowAlert } = props
 	return (
-		<Toast show={show} onClose={toggleShowAlert}>
-			<Toast.Header>
-				<div className={title === 'Error' ? 'alert_custom alert_danger' : 'alert_custom alert_warning'}></div>
-				<strong className='me-auto'>{title}</strong>
-				<small>{time}</small>
-			</Toast.Header>
-			<Toast.Body>{message}</Toast.Body>
-		</Toast>
+		<ToastContainer className='' position='top-right'>
+			<Toast show={show} onClose={toggleShowAlert} delay={5000} autohide animation={false}>
+				<Toast.Header>
+					<div className={title === 'Error' ? 'alert_custom alert_danger' : 'alert_custom alert_warning'}></div>
+					<strong className='me-auto'>{title}</strong>
+					<small>{time}</small>
+				</Toast.Header>
+				<Toast.Body>{message}</Toast.Body>
+			</Toast>
+		</ToastContainer>
 	)
 }
--- src/containers/appointmentPage/AppointmentPage.js
+++ src/containers/appointmentPage/AppointmentPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import CountElement from '../../components/countElement/CountElement'
 import TileList from '../../components/tileList/TileList'
+import AlertToasts from '../../components/alertToasts/AlertToasts'
 
 export default function AppointmentPage(props) {
 	const { appointments, contacts, addAppointment, removeAppointment } = props
@@ -11,15 +12,38 @@ export default function AppointmentPage(props) {
 	const [date, setDate] = useState('')
 	const [time, setTime] = useState('')
 	const [countAppointments, setCountAppointments] = useState(appointments.length)
+	const [showAlert, setShowAlert] = useState(false)
+	const [alertTitle, setAlertTitle] = useState('')
+	const [alertMessage, setAlertMessage] = useState('')
+	const [alertTime, setAlertTime] = useState('')
 
 	useEffect(() => {
 		setCountAppointments(appointments.length)
 	}, [appointments])
 
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		if (!title || !contact || !date || !time) {
+			setAlertTitle('Warning')
+			setAlertMessage('Missing fields')
+			setAlertTime('just now')
+			setShowAlert(true)
+			setTimeout(() => {
+				setShowAlert(false)
+			}, 5000)
+		}
+	}
+
+	const toggleShowAlert = () => {
+		setShowAlert(!showAlert)
+	}
+
 	return (
 		<Container fluid className='container_appointment'>
 			<CountElement title='Appointment' count={countAppointments} />
 			<TileList items={appointments} removeItems={removeAppointment} />
+			<AlertToasts show={showAlert} toggleShowAlert={toggleShowAlert} title={alertTitle} time={alertTime} message={alertMessage} />
+			<button onClick={handleSubmit}>SUMBIT</button>
 		</Container>
 	)
 }
