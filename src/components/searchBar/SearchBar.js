0440a634a3b3f276e42ca8c7a55eb58cc1cd5281
Templars <antoine.ratat@gmail.com>
2021-09-09 00:32:45 +0800

    searchbar component

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Form } from 'react-bootstrap'
+import './SearchBar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
@@ -7,8 +9,15 @@ library.add(fas)
 export default function SearchBar(props) {
 	const { type } = props
 	return (
-		<div>
-			<FontAwesomeIcon className='ml-1' size='1x' icon={['fas', 'search']} />
-		</div>
+		<>
+			<div class='input-group'>
+				<Form.Control type='text' placeholder='Search..' />
+				<div class='input-group-append'>
+					<div class='input-group-text' id='btnGroupAddon2'>
+						<FontAwesomeIcon className='ml-1 mr-1' size='1x' icon={['fas', 'search']} />
+					</div>
+				</div>
+			</div>
+		</>
 	)
 }
