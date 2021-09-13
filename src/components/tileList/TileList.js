a31564ce47464ea9248a1fae281f2b1b27a32067
Templars <antoine.ratat@gmail.com>
2021-09-13 15:55:26 +0800

    tilelist transition in TileList.css

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import Tile from '../tile/Tile'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
-
-{
-}
+import './TileList.css'
 
 export default function TileList({ items, removeItems, type }) {
-	// return items.map((item) => {
-	// 	return <Tile key={item.id} item={item} removeItem={removeItems} type={type} />
-	// })
 	return (
 		<>
 			<ReactCSSTransitionGroup transitionName='fade' transitionEnterTimeout={300} transitionLeaveTimeout={300}>
